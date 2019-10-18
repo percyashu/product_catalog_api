a1abdb5bd26783dc5c9c9e257fe16841cac09f33
percyashu <percyayukashu@gmail.com>
2019-10-18 08:32:56 +0100

    First commit in postgresCRUD

--- src/test/java/com/ashu/api/product_catalog_api/ProductCatalogApiApplicationTests.java
+++ src/test/java/com/ashu/api/product_catalog_api/ProductCatalogApiApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProductCatalogApiApplicationTests {
+public class ProductCatalogApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
