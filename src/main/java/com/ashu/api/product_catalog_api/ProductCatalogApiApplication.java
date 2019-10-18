4dde9619ba440ce451b5f0c48db6c39234414f6e
percyashu <percyayukashu@gmail.com>
2019-10-18 08:46:32 +0100

    First commit in postgresCRUD

--- src/main/java/com/ashu/api/product_catalog_api/ProductCatalogApiApplication.java
+++ src/main/java/com/ashu/api/product_catalog_api/ProductCatalogApiApplication.java
@@ -2,8 +2,12 @@ package com.ashu.api.product_catalog_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableJpaRepositories("com.ashu.api.product_catalog_api.repository")
 public class ProductCatalogApiApplication {
 
 	public static void main(String[] args) {
